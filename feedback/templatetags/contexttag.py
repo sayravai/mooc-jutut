27d2e5c6b0a93e1897800a000b7107dbee43a7f1
Seppo Äyräväinen <seppo.ayravainen@aalto.fi>
2025-11-06 15:07:08 +0200

    More fixes

--- feedback/templatetags/contexttag.py
+++ feedback/templatetags/contexttag.py
@@ -7,7 +7,7 @@ register = template.Library()
 
 def render_context_tag(tag, tooltip="", value="") -> str:
     attrs = {
-        'class': 'context-tag label ' + ('colortag-dark' if tag.font_white else 'colortag-light'),
+        'class': 'context-tag badge ' + ('colortag-dark' if tag.font_white else 'colortag-light'),
         'style': 'background-color: {};'.format(tag.color)
     }
     if tooltip:
